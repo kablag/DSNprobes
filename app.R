1fc11fabfd77ac207433c61b60793c3494dba991
kablag <k.blag@yandex.ru>
2020-03-13 12:27:01 +0300

    v1.1

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("DSN Probes"),
-
+    
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
@@ -27,13 +27,19 @@ ui <- fluidPage(
             sliderInput("probeMinMaxLength", "Probes Length", 
                         5, 40, c(15, 30)),
             sliderInput("probeMinMaxTm", "Probes Tm", 
-                        30, 70, c(48, 55)),
+                        30, 70, c(55, 65)),
             sliderInput("probeMinFlank", "Probe Min flanking Nucleotides", 
                         1, 10, 3),
             sliderInput("probeMaxOverlapTm", "Probes Overlap Max Tm", 
                         1, 60, 36),
             sliderInput("abProbesMaxDTm", "A-B Probes Max ∆Tm", 
-                        0, 20, 1.2, step = 0.1)
+                        0, 20, 1.2, step = 0.1),
+            wellPanel(
+                numericInput("oligoConc", "Oligo Conc, nM", 200),
+                numericInput("naConc", "Na+ Conc, mM", 50),
+                numericInput("mgConc", "Mg++ Conc, mM", 3),
+                numericInput("dntpsConc", "dNTPs Conc, mM", 0.8)
+            )
         ),
 
         # Show a plot of the generated distribution
@@ -112,7 +118,12 @@ server <- function(input, output) {
                 probes[
                     , probe := str_sub(wtSeq, probeStart, probeStop)
                     ][
-                        , wtTm := Tm_NN(probe),
+                        , wtTm := Tm_NN(probe, 
+                                        dnac1 = input$oligoConc,
+                                        dnac2 = input$oligoConc,
+                                        Na = input$naConc,
+                                        Mg = input$mgConc,
+                                        dNTPs = input$dntpsConc),
                         by = 1:NROW(probes)
                         ][
                             wtTm >= input$probeMinMaxTm[1] & wtTm <= input$probeMinMaxTm[2]
@@ -123,7 +134,12 @@ server <- function(input, output) {
             if (delInsType)
                 probes[, mTm := wtTm - DEL_INS_PENALTY_TM]
             else
-                probes[, mTm := Tm_NN(probe, comSeq = probe_m_comp),
+                probes[, mTm := Tm_NN(probe, comSeq = probe_m_comp, 
+                                      dnac1 = input$oligoConc,
+                                      dnac2 = input$oligoConc,
+                                      Na = input$naConc,
+                                      Mg = input$mgConc,
+                                      dNTPs = input$dntpsConc),
                        by = 1:length(probe)
                        ]
             probes[ , deltaTm := wtTm - mTm]
@@ -158,10 +174,18 @@ server <- function(input, output) {
         uniqueOverlaps <- unique(probes[, .(ABoverlap)])
         # for faster melting sort by start and stop.
         # Don't melt same start after ABoverlapTm > PROBES_MAX_OVERLAP_TM
-        uniqueOverlaps <- uniqueOverlaps[, ABoverlapTm := Tm_NN(ABoverlap),
-                                         by = 1:NROW(uniqueOverlaps)][
-                                             ABoverlapTm < input$probeMaxOverlapTm
-                                             ]
+        uniqueOverlaps <- uniqueOverlaps[
+            , ABoverlapTm := Tm_NN(ABoverlap, 
+                                   dnac1 = input$oligoConc,
+                                   dnac2 = input$oligoConc,
+                                   Na = input$naConc,
+                                   Mg = input$mgConc,
+                                   dNTPs = input$dntpsConc),
+            by = 1:NROW(uniqueOverlaps)][
+                ABoverlapTm < input$probeMaxOverlapTm
+                ][
+                    , ABoverlapL := str_length(ABoverlap)
+                ]
         
         probes <- merge(probes, uniqueOverlaps, 
                         by.x = "ABoverlap", by.y = "ABoverlap", 
@@ -177,10 +201,12 @@ server <- function(input, output) {
                 , probesTxt := 
                     sprintf(
                         paste("Probe A: %s, %i nt long, Tm = %.1f °C, ∆Tm(WT-M) = %.1f °C",
-                               "Probe B: %s, %i nt long, Tm = %.1f °C, ∆Tm(WT-M) = %.1f °C\n",
-                               "%s\n\n", sep = "\n"),
+                              "Probe B: %s, %i nt long, Tm = %.1f °C, ∆Tm(WT-M) = %.1f °C",
+                              "A-B Overlap: %s, %i nt long, Tm = %.1f °C\n",
+                              "%s\n\n", sep = "\n"),
                         probe_A, probeL_A, wtTm_A, deltaTm_A,
                         probe_B, probeL_B, wtTm_B, deltaTm_B,
+                        ABoverlap, ABoverlapL, ABoverlapTm,
                         paste(
                             str_pad(probe_A, width = probeStop_A, side = "left"),
                             wtSeq,
@@ -209,7 +235,10 @@ server <- function(input, output) {
                 "Probe B" = probe_A, 
                 "Length" = probeL_A, 
                 "Tm" = round(wtTm_A, 1), 
-                "∆Tm(WT-M)" = round(deltaTm_A, 1))]
+                "∆Tm(WT-M)" = round(deltaTm_A, 1),
+                "A-B Overlap" = ABoverlap,
+                "Length" = ABoverlapL,
+                "Tm" = round(ABoverlapTm, 1))]
         DT::datatable(
             data = dat,
             extensions = 'Buttons',
